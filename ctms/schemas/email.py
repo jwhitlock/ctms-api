dd3eb61aa4c3003d4f461f7dfc2ae0af244468f5
John Whitlock <jwhitlock@mozilla.com>
2021-06-15 17:22:33 -0500

    Convert email schema to Config.fields

--- ctms/schemas/email.py
+++ ctms/schemas/email.py
@@ -14,55 +14,16 @@ class EmailBase(ComparableBase):
     """Data that is included in input/output/db of a primary_email and such."""
 
     primary_email: str
-    basket_token: Optional[UUID] = Field(
-        default=None,
-        description="Basket token, Token__c in Salesforce",
-        example="c4a7d759-bb52-457b-896b-90f1d3ef8433",
-    )
-    double_opt_in: bool = Field(
-        default=False, description="User has clicked a confirmation link", example=True
-    )
-    sfdc_id: Optional[str] = Field(
-        default=None,
-        max_length=255,
-        description="Salesforce legacy ID, Id in Salesforce",
-        example="001A000023aABcDEFG",
-    )
-    first_name: Optional[str] = Field(
-        default=None,
-        max_length=255,
-        description="First name of contact, FirstName in Salesforce",
-        example="Jane",
-    )
-    last_name: Optional[str] = Field(
-        default=None,
-        max_length=255,
-        description="Last name of contact, LastName in Salesforce",
-        example="Doe",
-    )
-    mailing_country: Optional[str] = Field(
-        default=None,
-        max_length=255,
-        description="Mailing country code, 2 lowercase letters, MailingCountryCode in Salesforce",
-        example="us",
-    )
-    email_format: Literal["H", "T", "N", ""] = Field(
-        default="H",
-        description="Email format, H=HTML, T=Plain Text, N and Empty=No selection, Email_Format__c in Salesforce",
-    )
-    email_lang: Optional[str] = Field(
-        default="en",
-        max_length=5,
-        description="Email language code, usually 2 lowercase letters, Email_Language__c in Salesforce",
-    )
-    has_opted_out_of_email: bool = Field(
-        default=False,
-        description="User has opted-out, HasOptedOutOfEmail in Salesforce",
-    )
-    unsubscribe_reason: Optional[str] = Field(
-        default=None,
-        description="Reason for unsubscribing, in basket IGNORE_USER_FIELDS, Unsubscribe_Reason__c in Salesforce",
-    )
+    basket_token: Optional[UUID]
+    double_opt_in: bool = False
+    sfdc_id: Optional[str]
+    first_name: Optional[str]
+    last_name: Optional[str]
+    mailing_country: Optional[str]
+    email_format: Literal["H", "T", "N", ""] = "H"
+    email_lang: Optional[str] = "en"
+    has_opted_out_of_email: bool = False
+    unsubscribe_reason: Optional[str]
 
     class Config:
         orm_mode = True
@@ -70,36 +31,76 @@ class EmailBase(ComparableBase):
             "primary_email": {
                 "description": "Contact email address, Email in Salesforce",
                 "example": "contact@example.com",
-            }
+            },
+            "basket_token": {
+                "description": "Basket token, Token__c in Salesforce",
+                "example": "c4a7d759-bb52-457b-896b-90f1d3ef8433",
+            },
+            "double_opt_in": {
+                "description": "User has clicked a confirmation link",
+                "example": True,
+            },
+            "sfdc_id": {
+                "max_length": 255,
+                "description": "Salesforce legacy ID, Id in Salesforce",
+                "example": "001A000023aABcDEFG",
+            },
+            "first_name": {
+                "max_length": 255,
+                "description": "First name of contact, FirstName in Salesforce",
+                "example": "Jane",
+            },
+            "last_name": {
+                "max_length": 255,
+                "description": "Last name of contact, LastName in Salesforce",
+                "example": "Doe",
+            },
+            "mailing_country": {
+                "max_length": 255,
+                "description": "Mailing country code, 2 lowercase letters, MailingCountryCode in Salesforce",
+                "example": "us",
+            },
+            "email_format": {
+                "description": "Email format, H=HTML, T=Plain Text, N and Empty=No selection, Email_Format__c in Salesforce",
+            },
+            "email_lang": {
+                "max_length": 5,
+                "description": "Email language code, usually 2 lowercase letters, Email_Language__c in Salesforce",
+            },
+            "has_opted_out_of_email": {
+                "description": "User has opted-out, HasOptedOutOfEmail in Salesforce",
+            },
+            "unsubscribe_reason": {
+                "description": "Reason for unsubscribing, in basket IGNORE_USER_FIELDS, Unsubscribe_Reason__c in Salesforce",
+            },
         }
 
 
 class EmailSchema(EmailBase):
-    email_id: UUID4 = Field(
-        description=EMAIL_ID_DESCRIPTION,
-        example=EMAIL_ID_EXAMPLE,
-    )
-    create_timestamp: Optional[datetime] = Field(
-        default=None,
-        description="Contact creation date, CreatedDate in Salesforce",
-        example="2020-03-28T15:41:00.000Z",
-    )
-    update_timestamp: Optional[datetime] = Field(
-        default=None,
-        description="Contact last modified date, LastModifiedDate in Salesforce",
-        example="2021-01-28T21:26:57.511Z",
-    )
+    email_id: UUID4
+    create_timestamp: Optional[datetime]
+    update_timestamp: Optional[datetime]
+
+    class Config:
+        fields = {
+            "email_id": {
+                "description": EMAIL_ID_DESCRIPTION,
+                "example": EMAIL_ID_EXAMPLE,
+            },
+            "create_timestamp": {
+                "description": "Contact creation date, CreatedDate in Salesforce",
+                "example": "2020-03-28T15:41:00.000Z",
+            },
+            "update_timestamp": {
+                "description": "Contact last modified date, LastModifiedDate in Salesforce",
+                "example": "2021-01-28T21:26:57.511Z",
+            },
+        }
 
 
 class EmailTableSchema(EmailSchema):
-    create_timestamp: datetime = Field(
-        description="Contact creation date, CreatedDate in Salesforce",
-        example="2020-03-28T15:41:00.000Z",
-    )
-    update_timestamp: datetime = Field(
-        description="Contact last modified date, LastModifiedDate in Salesforce",
-        example="2021-01-28T21:26:57.511Z",
-    )
+    create_timestamp: datetime
+    update_timestamp: datetime
 
     class Config:
         extra = "forbid"
@@ -108,20 +109,29 @@ class EmailTableSchema(EmailSchema):
 class EmailInSchema(EmailBase):
     """Nearly identical to EmailPutSchema but the email_id is not required."""
 
-    email_id: Optional[UUID4] = Field(
-        default=None,
-        description=EMAIL_ID_DESCRIPTION,
-        example=EMAIL_ID_EXAMPLE,
-    )
+    email_id: Optional[UUID4]
+
+    class Config:
+        fields = {
+            "email_id": {
+                "description": EMAIL_ID_DESCRIPTION,
+                "example": EMAIL_ID_EXAMPLE,
+            },
+        }
 
 
 class EmailPutSchema(EmailBase):
     """Nearly identical to EmailInSchema but the email_id is required."""
 
-    email_id: UUID4 = Field(
-        description=EMAIL_ID_DESCRIPTION,
-        example=EMAIL_ID_EXAMPLE,
-    )
+    email_id: UUID4
+
+    class Config:
+        fields = {
+            "email_id": {
+                "description": EMAIL_ID_DESCRIPTION,
+                "example": EMAIL_ID_EXAMPLE,
+            },
+        }
 
 
 class EmailPatchSchema(EmailInSchema):
@@ -138,7 +148,13 @@ class EmailPatchSchema(EmailInSchema):
 
 class UpdatedEmailPutSchema(EmailPutSchema):
     update_timestamp: datetime = Field(
-        default_factory=lambda: datetime.now(timezone.utc),
-        description="Contact last modified date, LastModifiedDate in Salesforce",
-        example="2021-01-28T21:26:57.511Z",
+        default_factory=lambda: datetime.now(timezone.utc)
     )
+
+    class Config:
+        fields = {
+            "update_timestamp": {
+                "description": "Contact last modified date, LastModifiedDate in Salesforce",
+                "example": "2021-01-28T21:26:57.511Z",
+            },
+        }
