6d045d48500c440f1b18595f176a10a408147963
John Whitlock <jwhitlock@mozilla.com>
2021-06-15 16:31:34 -0500

    Convert mofo schema to Config.fields

--- ctms/schemas/mofo.py
+++ ctms/schemas/mofo.py
@@ -1,28 +1,29 @@
 from typing import Optional
 
-from pydantic import Field
-
 from .base import ComparableBase
 
 
 class MozillaFoundationBase(ComparableBase):
 
-    mofo_email_id: Optional[str] = Field(
-        default=None,
-        max_length=255,
-        description="Foriegn key to email in MoFo contact database",
-    )
-    mofo_contact_id: Optional[str] = Field(
-        default=None,
-        max_length=255,
-        description="Foriegn key to contact in MoFo contact database",
-    )
-    mofo_relevant: bool = Field(
-        default=False, description="Mozilla Foundation is tracking this email"
-    )
+    mofo_email_id: Optional[str]
+    mofo_contact_id: Optional[str]
+    mofo_relevant: bool = False
 
     class Config:
         orm_mode = True
+        fields = {
+            "mofo_email_id": {
+                "max_length": 255,
+                "description": "Foriegn key to email in MoFo contact database",
+            },
+            "mofo_contact_id": {
+                "max_length": 255,
+                "description": "Foriegn key to contact in MoFo contact database",
+            },
+            "mofo_relevant": {
+                "description": "Mozilla Foundation is tracking this email",
+            },
+        }
 
 
 MozillaFoundationSchema = MozillaFoundationBase
