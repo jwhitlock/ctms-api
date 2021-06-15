c068add5a469d636900624aa25a69931bd8e0dff
John Whitlock <jwhitlock@mozilla.com>
2021-06-15 16:40:06 -0500

    Convert newsletter schema to Config.fields

--- ctms/schemas/newsletter.py
+++ ctms/schemas/newsletter.py
@@ -10,36 +10,40 @@ from .email import EMAIL_ID_DESCRIPTION, EMAIL_ID_EXAMPLE
 class NewsletterBase(ComparableBase):
     """The newsletter subscriptions schema."""
 
-    name: str = Field(
-        description="Basket slug for the newsletter",
-        example="mozilla-welcome",
-    )
-    subscribed: bool = Field(
-        default=True, description="True if subscribed, False when formerly subscribed"
-    )
-    format: Literal["H", "T"] = Field(
-        default="H", description="Newsletter format, H=HTML, T=Plain Text"
-    )
-    lang: Optional[str] = Field(
-        default="en",
-        min_length=2,
-        max_length=5,
-        description="Newsletter language code, usually 2 lowercase letters",
-    )
-    source: Optional[HttpUrl] = Field(
-        default=None,
-        description="Source URL of subscription",
-        example="https://www.mozilla.org/en-US/",
-    )
-    unsub_reason: Optional[str] = Field(
-        default=None, description="Reason for unsubscribing"
-    )
+    name: str
+    subscribed: bool = True
+    format: Literal["H", "T"] = "H"
+    lang: Optional[str] = "en"
+    source: Optional[HttpUrl]
+    unsub_reason: Optional[str]
 
     def __lt__(self, other):
         return self.name < other.name
 
     class Config:
         orm_mode = True
+        fields = {
+            "name": {
+                "description": "Basket slug for the newsletter",
+                "example": "mozilla-welcome",
+            },
+            "subscribed": {
+                "description": "True if subscribed, False when formerly subscribed",
+            },
+            "format": {"description": "Newsletter format, H=HTML, T=Plain Text"},
+            "lang": {
+                "min_length": 2,
+                "max_length": 5,
+                "description": "Newsletter language code, usually 2 lowercase letters",
+            },
+            "source": {
+                "description": "Source URL of subscription",
+                "example": "https://www.mozilla.org/en-US/",
+            },
+            "unsub_reason": {
+                "description": "Reason for unsubscribing",
+            },
+        }
 
 
 # No need to change anything, just extend if you want to
@@ -50,24 +54,35 @@ NewsletterSchema = NewsletterBase
 class UpdatedNewsletterInSchema(NewsletterInSchema):
     update_timestamp: datetime = Field(
         default_factory=lambda: datetime.now(timezone.utc),
-        description="Newsletter subscription data update timestamp",
-        example="2021-01-28T21:26:57.511Z",
-    )
-
-
-class NewsletterTableSchema(NewsletterBase):
-    email_id: UUID4 = Field(
-        description=EMAIL_ID_DESCRIPTION,
-        example=EMAIL_ID_EXAMPLE,
-    )
-    create_timestamp: datetime = Field(
-        description="Newsletter subscription data creation timestamp",
-        example="2020-12-05T19:21:50.908000+00:00",
-    )
-    update_timestamp: datetime = Field(
-        description="Newsletter subscription data update timestamp",
-        example="2021-02-04T15:36:57.511000+00:00",
     )
 
     class Config:
+        fields = {
+            "update_timestamp": {
+                "description": "Newsletter subscription data update timestamp",
+                "example": "2021-01-28T21:26:57.511Z",
+            }
+        }
+
+
+class NewsletterTableSchema(NewsletterBase):
+    email_id: UUID4
+    create_timestamp: datetime
+    update_timestamp: datetime
+
+    class Config:
         extra = "forbid"
+        fields = {
+            "email_id": {
+                "description": EMAIL_ID_DESCRIPTION,
+                "example": EMAIL_ID_EXAMPLE,
+            },
+            "create_timestamp": {
+                "description": "Newsletter subscription data creation timestamp",
+                "example": "2020-12-05T19:21:50.908000+00:00",
+            },
+            "update_timestamp": {
+                "description": "Newsletter subscription data update timestamp",
+                "example": "2021-01-28T21:26:57.511Z",
+            },
+        }
